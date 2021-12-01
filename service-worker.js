f9453f4894d75ad51f580983b8c762df5767be5b
jan-martinek <honza.martinek@gmail.com>
2021-12-01 17:06:51 +0000

    Deploying to docs from @ books-are-next/slon_a_jeho_strycek@cc5e008bb6de689cc76cc70843de751ddab81fde 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-0241c78';
+const CACHE = 'cache-cc5e008';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -39,7 +39,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./manifest.json","./index.html","./resources.html","./slon_a_jeho_strycek_001.html","./slon_a_jeho_strycek_003.html","./slon_a_jeho_strycek_005.html","./slon_a_jeho_strycek_006.html","./slon_a_jeho_strycek_007.html","./slon_a_jeho_strycek_008.html","./slon_a_jeho_strycek_009.html","./slon_a_jeho_strycek_010.html","./slon_a_jeho_strycek_011.html","./slon_a_jeho_strycek_012.html","./slon_a_jeho_strycek_013.html","./slon_a_jeho_strycek_014.html","./slon_a_jeho_strycek_015.html","./slon_a_jeho_strycek_016.html","./slon_a_jeho_strycek_017.html","./slon_a_jeho_strycek_018.html","./slon_a_jeho_strycek_019.html","./slon_a_jeho_strycek_020.html","./slon_a_jeho_strycek_021.html","./slon_a_jeho_strycek_022.html","./slon_a_jeho_strycek_023.html","./slon_a_jeho_strycek_024.html","./slon_a_jeho_strycek_025.html","./slon_a_jeho_strycek_026.html","./slon_a_jeho_strycek_027.html","./slon_a_jeho_strycek_028.html","./slon_a_jeho_strycek_029.html","./slon_a_jeho_strycek_030.html","./slon_a_jeho_strycek_031.html","./slon_a_jeho_strycek_032.html","./slon_a_jeho_strycek_033.html","./resources/image001.jpg","./resources/image002.jpg","./resources/index.xml","./resources/obalka_slon_a_jeho_strycek.jpg","./resources/obr1.jpg","./resources/obr11.jpg","./resources/obr2.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./resources.html","./slon_a_jeho_strycek_001.html","./slon_a_jeho_strycek_003.html","./slon_a_jeho_strycek_005.html","./slon_a_jeho_strycek_006.html","./slon_a_jeho_strycek_007.html","./slon_a_jeho_strycek_008.html","./slon_a_jeho_strycek_009.html","./slon_a_jeho_strycek_010.html","./slon_a_jeho_strycek_011.html","./slon_a_jeho_strycek_012.html","./slon_a_jeho_strycek_013.html","./slon_a_jeho_strycek_014.html","./slon_a_jeho_strycek_015.html","./slon_a_jeho_strycek_016.html","./slon_a_jeho_strycek_017.html","./slon_a_jeho_strycek_018.html","./slon_a_jeho_strycek_019.html","./slon_a_jeho_strycek_020.html","./slon_a_jeho_strycek_021.html","./slon_a_jeho_strycek_022.html","./slon_a_jeho_strycek_023.html","./slon_a_jeho_strycek_024.html","./slon_a_jeho_strycek_025.html","./slon_a_jeho_strycek_026.html","./slon_a_jeho_strycek_027.html","./slon_a_jeho_strycek_028.html","./slon_a_jeho_strycek_029.html","./slon_a_jeho_strycek_030.html","./slon_a_jeho_strycek_031.html","./slon_a_jeho_strycek_032.html","./slon_a_jeho_strycek_033.html","./resources/image001.jpg","./resources/image002.jpg","./resources/index.xml","./resources/obalka_slon_a_jeho_strycek.jpg","./resources/obr1.jpg","./resources/obr11.jpg","./resources/obr2.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css"]));
 }
 
 self.addEventListener('fetch', e => {
